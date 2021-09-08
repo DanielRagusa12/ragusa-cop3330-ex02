cb47eff46b2749cdc964f6a17d7f3e09a5e1dc85
DanielRagusa12 <daniel.ragusa129@gmail.com>
2021-09-08 15:16:38 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Daniel Ragusa
+ */
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.print("What is the input string? ");
+        String InputString;
+        Scanner input=new Scanner(System.in);
+        InputString=input.next();
+        int length=InputString.length();
+        System.out.println((InputString) + " has " + (InputString.length()) + " characters.");
+    }
+}
